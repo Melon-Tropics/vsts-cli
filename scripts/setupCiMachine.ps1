7967dbc9878dfb7f7ba2c7ee0bfe74b06d5028be
Gaurav Saral <gsaral@microsoft.com>
2018-12-19 12:02:25 +0530

    starting with 2.0.0

--- scripts/setupCiMachine.ps1
+++ scripts/setupCiMachine.ps1
@@ -19,7 +19,7 @@ Write-Output "done"
 
 # Install CLI & CLI testsdk
 Write-Output "installing azure-cli-testsdk and azure-cli..."
-pip install --pre azure-cli --extra-index-url https://azurecliprod.blob.core.windows.net/edge
+pip install --pre azure-cli==2.0.0
 # Update the git commit or branch when we need a new version of azure-cli-testsdk
 pip install "git+https://github.com/Azure/azure-cli@master#egg=azure-cli-testsdk&subdirectory=src/azure-cli-testsdk" -q
 Write-Output "done"
